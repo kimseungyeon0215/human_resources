d9f3aaeea5c2618abb54be6c333ed1bb50408c77
kimseungyeon0215 <sykim021500@gmail.com>
2026-01-19 13:14:21 +0900

    vite config host fix

--- hr-app/vite.config.ts
+++ hr-app/vite.config.ts
@@ -2,5 +2,13 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		allowedHosts: [
+			'xn--299a1v27n0w5a.store',     
+    		'www.xn--299a1v27n0w5a.store',
+			'xn--zb0brsr4zmlx.store',
+            '근태관리.store'
+		]
+	}
 });
